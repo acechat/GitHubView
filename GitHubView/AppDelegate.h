14e58c4e99cca1c4a99236bf64bcc198dfff267c
Sungju Kwon <sungju.kwon@gmail.com>
2013-09-19 20:21:32 +1000

    Initial Commit

---
+++ GitHubView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubView
+//
+//  Created by Sungju Kwon on 19/09/13.
+//  Copyright (c) 2013 Sungju Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubViewTests/GitHubViewTests.m
@@ -0,0 +1,34 @@
+//
+//  GitHubViewTests.m
+//  GitHubViewTests
+//
+//  Created by Sungju Kwon on 19/09/13.
+//  Copyright (c) 2013 Sungju Kwon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitHubViewTests : XCTestCase
+
+@end
+
+@implementation GitHubViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
